2feaf1baae0c143de7d962c30d8d0cd5cdae5fab
けもみみ愛してる <hirohiro.j3cub@gmail.com>
2025-02-19 15:10:07 +0900

    Adapter以下のコメント追加と微修正
    - Controllerで、 Interactor<TInput, TOutput> を扱っていたところを IUseCase<TInput, TOutput> を扱うように。これで必要十分。

--- MimiClean/MimiClean/Adapter/IController.cs
+++ MimiClean/MimiClean/Adapter/IController.cs
@@ -1,7 +1,14 @@
 ﻿namespace StudioIdGames.MimiClean.Adapter
 {
+    /// <summary>
+    /// <see cref="Controller{TInput, TOutput, TResult}"/>の操作を抽象化するためのインターフェースです。
+    /// </summary>
     public interface IController
     {
+        /// <summary>
+        /// この操作を実行し、結果を取得します。
+        /// </summary>
+        /// <returns>実行結果</returns>
         CleanResult<object> Invoke();
     }
 }
