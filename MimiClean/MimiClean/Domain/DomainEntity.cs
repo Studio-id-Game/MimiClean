4e4a877ad2c9fc8645fa197627520ab2150f815e
けもみみ愛してる <hirohiro.j3cub@gmail.com>
2025-02-19 15:00:30 +0900

    Domain以下のクラスにコメントを追加
    - ついでにModuleSetだけ、少し仕様を変更

--- MimiClean/MimiClean/Domain/DomainEntity.cs
+++ MimiClean/MimiClean/Domain/DomainEntity.cs
@@ -2,8 +2,12 @@
 
 namespace StudioIdGames.MimiClean.Domain
 {
+    /// <summary>
+    /// 概念層の設計に登場するオブジェクトを表す抽象クラスです
+    /// </summary>
     public abstract class DomainEntity : IDomainEntity
     {
+        /// <inheritdoc/>
         public virtual IEnumerable<T> M<T>() where T : class, IDomainModule
         {
             yield break;
--- MimiClean/MimiClean/Domain/IService.cs
+++ MimiClean/MimiClean/Domain/IService.cs
@@ -1,7 +1,7 @@
 ﻿namespace StudioIdGames.MimiClean.Domain
 {
     /// <summary>
-    /// ベクトル演算など、完全な依存性注入が困難なフレームワーク動作をサービスとして提供します。
+    /// ベクトル演算など、完全な依存性注入が困難なフレームワーク機能をサービスとして提供します。
     /// 代替可能でシングルトン（static）なサービスを抽象化する事が出来ます。
     /// <see cref="Service{TInterface, TSelf}"/>と共に使用します。
     /// </summary>
