f8d242e0ddb300490d0fdcb865ec1c91b4e3b7a5
けもみみ愛してる <hirohiro.j3cub@gmail.com>
2025-02-26 12:54:13 +0900

    Little update (#20)
    * Add Select mode sections to MimiClean.Sample
    
    * Add MimiCleanSetup class for easy to use.

---
+++ MimiClean/MimiClean/MimiCleanAppSetup.cs
@@ -0,0 +1,12 @@
+﻿using StudioIdGames.MimiCleanContainer;
+
+namespace StudioIdGames.MimiClean
+{
+    public static class MimiCleanSetup
+    {
+        public static void SetDefaultService(MimiServiceContainer container)
+        {
+            Domain.App.MimiCleanAppSetup.SetDefaultService(container);
+        }
+    }
+}
