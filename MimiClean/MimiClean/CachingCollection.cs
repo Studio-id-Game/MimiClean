1ff4ea0ce20098c9e7a40e897b5c1219ecf73467
けもみみ愛してる <hirohiro.j3cub@gmail.com>
2025-03-01 19:16:18 +0900

    Add RepositoryCleanResult~~ and CachingCollections.
    - for #14

---
+++ MimiClean/MimiClean/CachingCollection.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace StudioIdGames.MimiClean
+{
+    public abstract class CachingCollection<TValue> : CachingBase<int, TValue>, IReadOnlyCollection<CleanResultBoxed<TValue>>
+    {
+        public object this[int index] => GetValue(index);
+
+        public override sealed CleanResult<TValue> GetValue(int index)
+        {
+            if (index < 0 || Count <= index)
+            {
+                return CleanResult<TValue>.Failed(new IndexOutOfRangeException(nameof(index)));
+            }
+
+            return base.GetValue(index);
+        }
+
+        protected abstract override CleanResult<TValue> GetValueProtected(int index);
+
+        public IEnumerator<CleanResultBoxed<TValue>> GetEnumerator()
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                yield return GetValue(i).Box();
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
