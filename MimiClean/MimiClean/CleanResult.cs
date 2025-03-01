e96ca3d393fad9db10b33e57ee8b509b69daab88
けもみみ愛してる <hirohiro.j3cub@gmail.com>
2025-03-01 19:08:39 +0900

    Update CleanResult
    - for #38

--- MimiClean/MimiClean/CleanResult.cs
+++ MimiClean/MimiClean/CleanResult.cs
@@ -29,6 +29,24 @@
         /// </summary>
         /// <returns></returns>
         public static CleanResult<Void> Failed(CleanResultError error) => CleanResult<Void>.Failed(error);
+
+        /// <summary>
+        /// 操作の成功を通知する、戻り値のあるオブジェクトを返します。
+        /// </summary>
+        /// <returns></returns>
+        public static CleanResult<T> Success<T>(T value) => CleanResult<T>.Success(value);
+
+        /// <summary>
+        /// 操作の中断を通知する、戻り値のあるオブジェクトを返します。
+        /// </summary>
+        /// <returns></returns>
+        public static CleanResult<T> Canceled<T>(T value) => CleanResult<T>.Canceled(value);
+
+        /// <summary>
+        /// 操作の失敗を通知する、戻り値のあるオブジェクトを返します。
+        /// </summary>
+        /// <returns></returns>
+        public static CleanResult<T> Failed<T>(CleanResultError error) => CleanResult<T>.Failed(error);
     }
 
     /// <summary>
